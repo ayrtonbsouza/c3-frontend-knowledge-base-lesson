ec65d6a46268e9a79cd3949bce36dd9c237049bb
Ayrton Souza <ayrtonsouza@icloud.com>
2020-09-02 17:47:32 -0300

    initial commit

---
+++ src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <span>Copyright <strong>Ayrton de Souza ©</strong> 2010-2020</span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+  .footer {
+    grid-area: footer;
+    background-color: #DDD;
+    color: #333;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding-right: 10px;
+  }
+</style>
