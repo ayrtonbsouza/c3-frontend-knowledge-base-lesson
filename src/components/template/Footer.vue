3c15cd83ab276c5a67c9112bd216ac4228e5b204
Ayrton Souza <ayrtonsouza@icloud.com>
2020-09-03 16:18:33 -0300

    developing menu hidden and global state

--- src/components/template/Footer.vue
+++ src/components/template/Footer.vue
@@ -1,24 +1,31 @@
 <template>
   <footer>
-    <span>Copyright <strong>Ayrton de Souza ©</strong> 2010-2020</span>
+    <span>
+      Copyright <strong>Ayrton de Souza</strong> © 2010-2020
+    </span>
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'Footer'
-}
+  name: "Footer"
+};
 </script>
 
 <style>
-  .footer {
-    grid-area: footer;
-    background-color: #DDD;
-    color: #333;
+footer {
+  grid-area: footer;
+  background-color: #ddd;
+  color: #333;
+  width: 100%;
 
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    padding-right: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  
+}
+  span {
+    margin-right: 10px;
   }
+
 </style>
